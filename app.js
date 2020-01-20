d5bda7e4bb532a9edd2ae0d4c2c6450c298970c3
Abel Chiao <abelchiao@Abels-MacBook-Pro.local>
2020-01-20 12:47:32 -0800

    try to fix heroku compatibility

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -26,13 +34,6 @@ app.get("/", (req, res) => {
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-const path = require("path");
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}

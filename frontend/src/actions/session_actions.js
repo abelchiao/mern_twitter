423f95ee91e5d53d5a9788829b25af41c909f852
Abel Chiao <abelchiao@Abels-MacBook-Pro.local>
2020-01-20 14:12:25 -0800

    clean up code

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,8 +1,5 @@
-// Although there's only one function here so far, let's import the whole file since we will be adding more later
 import * as APIUtil from "../util/session_api_util";
 import jwt_decode from "jwt-decode";
-
-// This pattern should be familiar to you from the full stack project
 
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
